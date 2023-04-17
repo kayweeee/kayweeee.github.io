5f3e885b6e3a982dec07797278a718b36949d1d0
kayweeee <90853438+kayweeee@users.noreply.github.com>
2023-04-17 11:29:21 -0700

    added contact details in navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,10 +2,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import github from "../images/contactgithub.svg"
+import linkedin from "../images/linkedin.svg"
+import mail from "../images/contactmail.svg"
 
 function NavBar () {
     const [toggle, setToggle] = useState(false);
     const navigate = useNavigate();
+    const handleLinkedin =  () => {
+        window.open('https://www.linkedin.com/in/kay-wee-tan/')
+    };
+    const handleGithub = () => {
+        window.open('https://github.com/kayweeee')
+    };
 
     return (
         <div>
@@ -16,20 +25,38 @@ function NavBar () {
                 <FontAwesomeIcon icon={faBars} className="p-4" size="xl"/>
             </button>
             {toggle && (
-                <div className="top-0 left-0 w-[30vw] fixed p-5 md:px-10 pt-14 inline-flex flex-col space-y-7 h-screen 
-                bg-grey text-l lg:text-xl font-body z-30">
-                    <button
-                    onClick={ () => navigate('/')}
-                    >Home</button>
-                    <button
-                    onClick={ () => navigate('/aboutme')}
-                    >About</button>
-                    <button
-                    onClick={ () => navigate('/projects')}
-                    >Projects</button>
-                    <button
-                    onClick={ () => navigate('/contactme')}
-                    >Contact Me</button>
+                <div className="top-0 left-0 w-[30vw] fixed p-5 md:px-10 pt-14 flex flex-col h-screen bg-grey z-30 justify-between">
+                    <div className="flex flex-col space-y-7 font-bold text-xl lg:text-2xl font-body">
+                        <button
+                        onClick={ () => navigate('/')}
+                        >Home</button>
+                        <button
+                        onClick={ () => navigate('/aboutme')}
+                        >About</button>
+                        <button
+                        onClick={ () => navigate('/projects')}
+                        >Projects</button>
+                        <button
+                        onClick={ () => navigate('/contactme')}
+                        >Contact Me</button>
+                    </div>
+
+                    <div className="flex flex-row justify-end gap-4">
+                        <button className="pb-1"
+                        >
+                            <img src={mail} width={60}/>
+                        </button>
+                        <button
+                        onClick={handleLinkedin}
+                        >
+                            <img src={linkedin} width={50}/>
+                        </button>
+                        <button
+                        onClick={handleGithub}
+                        >
+                            <img src={github} width={50}/>
+                        </button>
+                    </div>
                 </div>
             )}
 

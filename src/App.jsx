e5567fbbaa3049d9660eed6542cc7cbd93c2b2a1
kayweeee <90853438+kayweeee@users.noreply.github.com>
2023-04-19 17:21:28 -0700

    scrolling behaviour

--- src/App.jsx
+++ src/App.jsx
@@ -16,73 +16,81 @@ function App () {
   return (
     <>
       <NavBar/> 
-      <div className="flex flex-col justify-center items-center h-screen">
-        <p className="text-5xl md:text-7xl font-display">KAY WEE</p>
-        <p>all things design</p>
-      </div>
 
-      <div className="h-screen flex flex-col justify-center space-y-5 md:grid md:grid-cols-2 md:gap-10 px-8 md:px-14 bg-slate-300">
-        <div className="flex items-center justify-center leading-normal md:leading-relaxed text-5xl md:text-7xl font-display">
-          <TypeIt
-          options={{
-            speed: 150, 
-            lifeLike: true,
-            waitUntilVisible: true,
-          }}
-          >Curious,<br/>
-          Ambitious,<br/>
-          Passionate.
-          </TypeIt>
-        </div>
-        <div className="flex items-center justify-center">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-        </div>
-      </div>
+      <div className="snap-y snap-mandatory h-screen overflow-scroll">
 
-      <div className="h-screen flex flex-col justify-center md:block">
-        <div className="p-5 md:p-10">
-          <p className="text-5xl md:text-7xl font-display">
-            Skills
-          </p>
-        </div>
-        <div className="grid grid-cols-4 md:grid-cols-6 gap-6 md:gap-14 px-5 md:px-10 py-3 ">
-          <div className="flex flex-col justify-center items-center py-0">
-            <img src={htmllogo}/>
-            <p>HTML</p>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img src={csslogo}/>
-            <p>CSS</p>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img src={jslogo}/>
-            <p>JavaScript</p>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img src={pythonlogo}/>
-            <p>Python</p>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img src={reactlogo}/>
-            <p>React</p>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img src={githublogo}/>
-            <p>Github</p>
-          </div>
-          <div className="flex flex-col justify-between items-center">
-            <img src={bootstraplogo}/>
-            <p>Bootstrap</p>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img src={tailwindlogo}/>
-            <p>Tailwind</p>
-          </div>
+        {/* intro screen */}
+        <div className="snap-start h-screen w-screen flex flex-col justify-center items-center">
+          <p className="text-5xl md:text-7xl font-display">KAY WEE</p>
+          <p>all things design</p>
         </div>
 
-        <div className="flex justify-end p-5 md:px-10">
-          <p>... with more in progress</p>
+      {/* about screen */}
+        <div className="snap-start h-screen w-screen flex flex-col justify-center space-y-5 md:grid md:grid-cols-2 md:gap-10 px-8 md:px-14 bg-slate-300">
+          <div className="flex items-center justify-center leading-normal md:leading-relaxed text-5xl md:text-7xl font-display">
+            <TypeIt
+            options={{
+              speed: 150, 
+              lifeLike: true,
+              waitUntilVisible: true,
+            }}
+            >Curious,<br/>
+            Ambitious,<br/>
+            Passionate.
+            </TypeIt>
+          </div>
+          <div className="flex items-center justify-center">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          </div>
         </div>
+        
+        {/* skill screen */}
+        <div className="snap-start h-screen w-screen overflow-scroll flex flex-col justify-center md:block">
+          <div className="p-5 md:p-10">
+            <p className="text-5xl md:text-7xl font-display">
+              Skills
+            </p>
+          </div>
+          <div className="grid grid-cols-4 md:grid-cols-6 gap-6 md:gap-14 px-5 md:px-10 py-3 ">
+            <div className="flex flex-col justify-center items-center py-0">
+              <img src={htmllogo}/>
+              <p>HTML</p>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <img src={csslogo}/>
+              <p>CSS</p>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <img src={jslogo}/>
+              <p>JavaScript</p>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-5">
+              <img src={pythonlogo}/>
+              <p>Python</p>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-6">
+              <img src={reactlogo}/>
+              <p>React</p>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-5">
+              <img src={githublogo}/>
+              <p>Github</p>
+            </div>
+            <div className="flex flex-col justify-between items-center">
+              <img src={bootstraplogo}/>
+              <p>Bootstrap</p>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <img src={tailwindlogo}/>
+              <p>Tailwind</p>
+            </div>
+          </div>
+
+          <div className="flex justify-end p-5 md:px-10">
+            <p>... with more in progress</p>
+          </div>
+        </div>
+
       </div>
     </>
   )

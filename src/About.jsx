76af3cb7e8e634f1d5f35e3346edbea1209946ad
kayweeee <90853438+kayweeee@users.noreply.github.com>
2023-04-21 10:42:26 -0700

    aboutme first page done

--- src/About.jsx
+++ src/About.jsx
@@ -5,20 +5,28 @@ function About () {
     return(
         <>
         <div className="snap-y snap-mandatory h-screen overflow-scroll">
-            <div className="snap-start h-screen w-screen overflow-hidden flex flex-col justify-center items-center gap-4">
-                <p className="text-3xl md:text-4xl underline underline-offset-8 font-display pb-3">About Me</p>
-                <div className="flex flex-col justify-center items-center md:flex-row md:gap-5 md:px-10">
-                    <div className="w-3/4 md:w-1/2">
-                        <img src={pic1}
-                        className="rounded"
-                        />
+            <div className="snap-start w-screen h-screen flex flex-col justify-center">
+                <div className="text-2xl md:text-4xl underline underline-offset-8 pl-10 md:pl-16 pb-4 md:pb-8 font-display">
+                        About Me
                     </div>
-                    <div className="w-3/4 md:w-1/2">
-                        <p className="text-2xl text-grey font-display pt-3 pb-2">explorer and learner</p>
-                        <p className="text-md">hi! welcome to my website, where i document my journey  to becoming a full-stack software engineer. </p>
+                <div className="flex flex-col md:flex-row items-center justify-center gap-5 px-10 pb-10">
+                    <div className="md:w-1/2 flex justify-center md:justify-end">
+                        <img src={pic1} className="md:w-1/2 rounded"/>
+                    </div>
+
+                    <div className="md:w-1/2 text-start">
+                        <div className="md:w-1/2">
+                            <p className="text-lg md:text-2xl text-grey font-display">
+                                explorer and learner
+                            </p>
+                            <p className="text-base md:text-lg">
+                                hi! welcome to my website, where i document my journey  to becoming a full-stack software engineer. 
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
+    
             
             <div className="snap-start h-screen flex flex-col justify-center items-center bg-grey">
                 <p>Education</p>
